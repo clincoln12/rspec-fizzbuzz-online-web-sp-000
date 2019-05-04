1591e4ff696c6b1ffd37015e73a76aaa823cb4eb
Chris Lincoln <chris.lincoln12@gmail.com>
2019-05-04 02:30:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,5 @@
 def fizzbuzz(int)
+<<<<<<< HEAD
   if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
   elsif int % 3 == 0
@@ -7,5 +8,9 @@ def fizzbuzz(int)
     "Buzz"
   else int % 4 == 0
     return nil
+=======
+  if int % 3 == 0
+    "Fizz"
+>>>>>>> cafbc1a0d228c821eba9428b1f6e7680394babd6
   end
 end
